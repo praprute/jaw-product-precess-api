4401d171d44bda2294fa72125fda72f49ce2c4ce
junior <trader.junior492047@gmail.com>
2023-05-13 00:39:12 +0700

    qwss

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "./src/app.ts",
       watch: true,
       autorestart: true,
+      args: "run dev",
       env: {
         PORT: 1337,
         NODE_ENV: "development",
